afe78e2791f2a07d2351a501db6833f4f2b03b66
matthew allison restin <144450180+maddycode15@users.noreply.github.com>
2023-10-04 12:16:29 -0300

    current working

--- Assets/scripts/CarDrive.cs
+++ Assets/scripts/CarDrive.cs
@@ -12,6 +12,8 @@ public class CarDrive : MonoBehaviour
 
     public float gravity;
 
+  
+
     private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -33,12 +35,18 @@ public class CarDrive : MonoBehaviour
             rb.AddRelativeForce(-Vector3.forward * stop);
         }
 
+        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
+        localVelocity.x = 0;
+        rb.velocity = transform.TransformDirection(localVelocity);
+
         if (Input.GetKey(KeyCode.D)) 
         {
             rb.AddTorque(Vector3.up * turnSpeed);
         }
 
-        if (Input.GetKey(KeyCode.A))
+       
+
+            if (Input.GetKey(KeyCode.A))
         {
             rb.AddTorque(Vector3.down * turnSpeed);
         }
